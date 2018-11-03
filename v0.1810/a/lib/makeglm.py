e77a039a587f37fb2f2bb81735efe94807f2cc53
FSL User <fsl@fslvm6.localdomain>
2018-11-03 17:31:43 +0000

    worked in glm

--- v0.1810/a/lib/makeglm.py
+++ v0.1810/a/lib/makeglm.py
@@ -4,10 +4,28 @@
 from os import system
 
 
-def PerformGLM():
-    PATH = '/home/fsl/Desktop/'
-    def make_fsgd_2groups(variable,d_subjid):
-        file = PATH+'file2groups.fsgd'
+class PerformGLM():
+    def __init__(self):
+        self.PATH = '/home/fsl/Desktop/'
+        self.local_maindir = '/usr/local/'
+
+        hemi = ['lh','rh']
+        thresh = [10,]
+        meas = ['thickness',]
+        contrast = {'Avg-Intercept':'1 0', 'Diff-Intercept':'0 1'}
+        name = ['Avg-Intercept','Diff-Intercept',]
+        sim_direction = ['neg', 'pos', 'abs']
+
+        self.RUN_GLM(self.make_fsgd_1group(), thresh[0], meas[0], name[0], hemi[0], self.make_contrasts(hemi[0], name[0], meas[0], contrast[name[0]]),self.PATH+'fsglm')
+    # 'lh-Avg-Intercept-thickness.mat': '1.00000 +0.00000 ')#Does the average thickness/area differ from zero?
+    # 'lh-Diff-f-m-Intercept-thickness.mat': '0.00000 1.00000 ')#Does the correlation between thickness/area and Value differ from zero?
+    #the slope is the change of thickness with age
+    #the intercepts/offset is the thicknes at age=0
+    #DODS = different offset different slope
+
+
+    def make_fsgd_2groups(self, variable,d_subjid):
+        file = self.PATH+'file2groups.fsgd'
         with open(file, 'a') as f:
             f.write('GroupDescriptorFile 1\nClass Class1 plus blue\nClass Class2 circle green\nVariables ')
             f.write(variable+'\n')
@@ -16,8 +34,8 @@ def PerformGLM():
             f.write('DefaultVariable Age\n')
         return file
 
-    def make_fsgd_1group():
-        file = PATH+'davide_dyst.fsgd'#file1group.fsgd'
+    def make_fsgd_1group(self):
+        file = self.PATH+'y.fsgd'#file1group.fsgd'
         #with open(file, 'a') as f:
         #    f.write('
         #            GroupDescriptorFile 1\n
@@ -29,32 +47,25 @@ def PerformGLM():
         #            DefaultVariable Age\n')
         return file
 
-    def make_contrasts(hemi, name, meas, contrast):
+    def make_contrasts(self, hemi, name, meas, contrast):
         file = hemi+'-'+name+'-'+meas+'.mat'
         with open(file, 'a') as f:
             f.write(contrast)
         return file
 
-    def RUN_GLM(fsgd, thresh, meas, name, hemi, mat):
+    def RUN_GLM(self, fsgd, thresh, meas, name, hemi, mat, glmdir):
         system('mris_preproc --fsgd '+fsgd+' --cache-in thickness.fwhm'+str(thresh)+'.fsaverage --target fsaverage --hemi '+hemi+' --out '+hemi+'.'+name+'.'+meas+'.'+str(thresh)+'.mgh')
-        system('mri_glmfit --y '+hemi+'.'+name+'.'+meas+'.'+str(thresh)+'.mgh --fsgd '+fsgd+' dods --glmdir Untitled --surf fsaverage '+hemi+' --label /usr/local/freesurfer/subjects/fsaverage/label/lh.aparc.label --C '+mat)
+        system('mri_glmfit --y '+hemi+'.'+name+'.'+meas+'.'+str(thresh)+'.mgh --fsgd '+fsgd+' dods --glmdir '+glmdir+' --surf fsaverage '+hemi+' --label '+self.local_maindir+'freesurfer/subjects/fsaverage/label/lh.aparc.label --C '+mat)
 
-    def make_images_results():
+    def RUN_sim(self, glmdir,sim_direction):
+        system('--glmdir '+glmdir+' --cache 4 '+sim_direction+' --cwp 0.05 --2spaces')
+
+    def make_images_results(self):
         with open('run.txt', 'a') as f:
             f.write('-v /usr/local/freesurfer/subjects/qdec/corrt1t1/lh-Avg-thickness/sig.mgh\n'+
                 '-viewport sagittal -slice 80 127, 127 -ss sag1\n'+
                 '-viewport sagittal -slice 127 127, 127 -ss sag2\n'+
                 '-quit')
-
-    hemi = ['lh','rh']
-    thresh = [10,]
-    meas = ['thickness',]
-    contrast = {'Avg-Intercept':'1 0', 'Diff-Intercept':'0 1'}
-    name = ['Avg-Intercept','Diff-Intercept',]
-
-    RUN_GLM(make_fsgd_1group(), thresh[0], meas[0], name[0], hemi[0], make_contrasts(hemi[0], name[0], meas[0], contrast[name[0]]))
-    # 'lh-Avg-Intercept-thickness.mat': '1.00000 +0.00000 ')#Does the average thickness/area differ from zero?
-    # 'lh-Diff-f-m-Intercept-thickness.mat': '0.00000 1.00000 ')#Does the correlation between thickness/area and Value differ from zero?
 
 
 
