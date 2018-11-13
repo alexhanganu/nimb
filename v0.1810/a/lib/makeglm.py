42e2f78cd31fb5bb2c07e4837b81fd616be1d276
FSL User <fsl@fslvm6.localdomain>
2018-11-13 17:58:10 +0000

    waiting for fsgd

--- v0.1810/a/lib/makeglm.py
+++ v0.1810/a/lib/makeglm.py
@@ -1,65 +1,27 @@
 #!/bin/python
-#Alexandru Hanganu, 2018 Mar 29
 
 from os import system
 
 
 class PerformGLM():
-    def __init__(self):
-        self.PATH = '/home/fsl/Desktop/'
-        self.local_maindir = '/usr/local/'
+    def __init__(self, table, PATH, local_maindir):
+        self.PATH = PATH
+        self.local_maindir = local_maindir
 
         hemi = ['lh','rh']
         thresh = [10,]
         meas = ['thickness',]
         gd2mtx = ['doss','dods']
         contrast2groups = {'Avg-Intercept':'+1.00000 +1.00000', 'Diff-groups-Intercept':'+1.00000 -1.00000'}
-        contrast = {'Avg-Intercept':'1 0', 'Diff-Intercept':'+0.00000 +1.00000 '}
+        contrast = {'Avg-Intercept':'+1.00000 +0.00000', 'Diff-Intercept':'+0.00000 +1.00000 '}
         name = ['Avg-Intercept','Diff-Intercept', 'Diff-groups-Intercept']
         sim_direction = ['neg', 'pos', 'abs']
         glmdir = self.PATH+'fsglm'
 
-        self.RUN_GLM(self.make_fsgd_1group(), 
+        self.RUN_GLM(self.make_fsgd(table), 
                      thresh[0],
                      glmdir, hemi[0], name[2], meas[0], gd2mtx[1],
                      self.make_contrasts(glmdir, name[2], contrast2groups[name[2]]))
-    # 'lh-Avg-Intercept-thickness.mat': '1.00000 +0.00000 ')#Does the average thickness/area differ from zero?
-    # 'lh-Diff-f-m-Intercept-thickness.mat': '0.00000 1.00000 ')#Does the correlation between thickness/area and Value differ from zero?
-    #the slope is the change of thickness with age
-    #the intercepts/offset is the thicknes at age=0
-    #DODS = different offset different slope
-
-
-    def make_fsgd_2groups(self, variable,d_subjid):
-        file = self.PATH+'qdec.fsgd'
-        with open(file, 'a') as f:
-            f.write('GroupDescriptorFile 1\nClass Class1 plus blue\nClass Class2 circle green\nVariables ')
-            f.write(variable+'\n')
-            for subjid in d_subjid:
-            	f.write('Input '+subjid+' '+d_subjid[subjid][0]+' '+d_subjid[subjid][1]+'\n')
-            f.write('DefaultVariable Age\n')
-        return file
-
-    def make_fsgd_1group(self):
-        file = self.PATH+'qdec.fsgd'#file1group.fsgd'
-        #with open(file, 'a') as f:
-        #    f.write('
-        #            GroupDescriptorFile 1\n
-        #            Class Main plus blue\n\n
-        #            Variables Age\n
-        #            Input subjid1 Main 10\n
-        #            Input subjid2 Main 20\n
-        #            Input subjid3 Main 20\n
-        #            DefaultVariable Age\n')
-        return file
-
-    def make_contrasts(self, glmdir, contrast_name, contrast):
-        file = glmdir+'/'+contrast_name+'.mat'
-        open(file, 'w').close()
-        with open(file, 'a') as f:
-            print('writing to C1.mat')
-            f.write(contrast)
-        return file
 
     def RUN_GLM(self, fsgd, thresh, glmdir, hemi, name, meas, gd2mtx, mat):
         mgh_f = glmdir+'/'+hemi+'.'+name+'.'+meas+'.'+str(thresh)+'.mgh'
@@ -80,18 +42,3 @@ class PerformGLM():
             for line in cmds:
                 f.write(line+'\n')
         system('tksurfer fsaverage '+hemi+' inflated -overlay '+glmdir+'/'+contrast_name+'/sig.mgh -fthresh 3.0 -tcl scr.tcl')
-
-
-
-
-
-
-# X = np.linspace(-5.0, 5.0, 100)
-# plt.title("PDF from Template")
-# data = scipy.stats.norm.rvs(size=100000, loc=0, scale=1.5, random_state=123)
-# histdata = np.histogram(data, bins=100)
-# hist_distdata = scipy.stats.rv_histogram(histdata)
-# plt.hist(data, normed=True, bins=100)
-# plt.plot(X, hist_distdata.pdf(X), label='PDF')
-# plt.plot(X, hist_distdata.cdf(X), label='CDF')
-# plt.show()
