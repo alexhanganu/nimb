beaf4326e979681c3324a873fb44a03d81b66bb0
alexhanganu <alexandru@hanganu.ca>
2020-06-25 15:26:11 -0400

    m

--- v02003/a/clib/remote_runglm.py
+++ v02003/a/clib/remote_runglm.py
@@ -9,13 +9,11 @@ import shutil, linecache, sys
 class PerformGLM():
     def __init__(self, PATHglm, FREESURFER_HOME, SUBJECTS_DIR):
         self.SUBJECTS_DIR = SUBJECTS_DIR
-        for file in listdir(PATHglm):
+        self.PATHglm = PATHglm
+
+        for file in listdir(self.PATHglm):
             if '.py' in file:
-                shutil.copy(path.join(PATHglm,file), path.join(getcwd(),file))
-        self.PATH = PATHglm
-        self.glmPATH = path.join(PATHglm,'glm/')
-        if not path.isdir(self.glmPATH):
-            makedirs(self.glmPATH)
+                shutil.copy(path.join(self.PATHglm,file), path.join(getcwd(),file))
         try:
             from subjects_per_group import subjects_per_group
             from files_for_glm import files_for_glm
@@ -39,33 +37,38 @@ class PerformGLM():
             
 
     def fsgd_win_to_unix(self, files_for_glm):
-        if not path.isdir(self.PATH+'fsgd_unix'):
-            makedirs(self.PATH+'fsgd_unix')
+        if not path.isdir(self.PATHglm+'fsgd_unix'):
+            makedirs(self.PATHglm+'fsgd_unix')
         for contrast_type in files_for_glm:
             for fsgd_file in files_for_glm[contrast_type]['fsgd']:
-                fsgd_f_unix = self.PATH+'fsgd_unix/'+fsgd_file.replace('.fsgd','')+'_unix.fsgd'
+                fsgd_f_unix = path.join(self.PATHglm,'fsgd_unix',fsgd_file.replace('.fsgd','')+'_unix.fsgd')
                 if not path.isfile(fsgd_f_unix):
-                    system('cat '+self.PATH+'fsgd/'+fsgd_file+' | sed \'s/\\r/\\n/g\' > '+fsgd_f_unix)
+                    system('cat '+path.join(self.PATHglm,'fsgd',fsgd_file)+' | sed \'s/\\r/\\n/g\' > '+fsgd_f_unix)
 
     def RUN_GLM(self, files_for_glm):
-        print('running')
+        print('performe GLM analysis using mri_glmfit')
+
+        PATHglm_glm = path.join(self.PATHglm,'glm/')
+        if not path.isdir(PATHglm_glm):
+            makedirs(PATHglm_glm)
+
         hemispheres = ['lh','rh']
         measurements = ['thickness','area','volume',]#'curv']
         thresholds = [10,]#5,15,20,25]
         for contrast_type in files_for_glm:
             for fsgd_file in files_for_glm[contrast_type]['fsgd']:
-                fsgd_f_unix = self.PATH+'fsgd_unix/'+fsgd_file.replace('.fsgd','')+'_unix.fsgd'
+                fsgd_f_unix = path.join(self.PATHglm,'fsgd_unix',fsgd_file.replace('.fsgd','')+'_unix.fsgd')
                 for hemi in hemispheres:
                     for meas in measurements:
                         for thresh in thresholds:
                             analysis_name = fsgd_file.replace('.fsgd','')+'.'+meas+'.'+hemi+'.fwhm'+str(thresh)
-                            glmdir = self.glmPATH+analysis_name
-                            mgh_f = glmdir+'/'+meas+'.'+hemi+'.fwhm'+str(thresh)+'.y.mgh'
+                            glmdir = path.join(PATHglm_glm,analysis_name)
+                            mgh_f = path.join(glmdir,meas+'.'+hemi+'.fwhm'+str(thresh)+'.y.mgh')
                             system('mris_preproc --fsgd '+fsgd_f_unix+' --cache-in '+meas+'.fwhm'+str(thresh)+'.fsaverage --target fsaverage --hemi '+hemi+' --out '+mgh_f)
                             for contrast in files_for_glm[contrast_type]['mtx']:
                                 explanation = files_for_glm[contrast_type]['mtx_explanation'][files_for_glm[contrast_type]['mtx'].index(contrast)]
                                 for gd2mtx in files_for_glm[contrast_type]['gd2mtx']:
-                                    system('mri_glmfit --y '+mgh_f+' --fsgd '+fsgd_f_unix+' '+gd2mtx+' --glmdir '+glmdir+' --surf fsaverage '+hemi+' --label '+self.SUBJECTS_DIR+'/fsaverage/label/'+hemi+'.aparc.label --C '+self.PATH+'contrasts/'+contrast)
+                                    system('mri_glmfit --y '+mgh_f+' --fsgd '+fsgd_f_unix+' '+gd2mtx+' --glmdir '+glmdir+' --surf fsaverage '+hemi+' --label '+path.join(self.SUBJECTS_DIR,'fsaverage','label',hemi+'.aparc.label')+' --C '+path.join(self.PATHglm,'contrasts',contrast))
                                     self.RUN_sim(glmdir, contrast.replace('.mtx',''), hemi, contrast_type, analysis_name, meas, explanation)
                                     # if self.check_maxvox(glmdir, contrast.replace('.mtx','')):
                                         # self.make_images_results_fdr(hemi, glmdir, contrast.replace('.mtx',''), 'sig.mgh', 3.0)
@@ -91,7 +94,7 @@ class PerformGLM():
         fwhm = {'thickness': {'lh': '15','rh': '15'},'area': {'lh': '24','rh': '25'},'volume': {'lh': '16','rh': '16'},}
         for direction in sim_direction:
             if meas != 'curv':
-                csd_mc_f = self.FREESURFER_HOME+'/average/mult-comp-cor/fsaverage/'+hemi+'/cortex/fwhm'+fwhm[meas][hemi]+'/'+direction+'/th'+str(cache)+'/mc-z.csd'
+                csd_mc_f = path.join(self.FREESURFER_HOME,'average','mult-comp-cor','fsaverage',hemi,'cortex','fwhm'+fwhm[meas][hemi],direction,'th'+str(cache),'mc-z.csd')
                 sig_f = contrastdir+'sig.mgh'
                 cwsig_mc_f = contrastdir+'mc-z.'+direction+'.th'+str(cache)+'.sig.cluster.mgh'
                 vwsig_mc_f = contrastdir+'mc-z.'+direction+'.th'+str(cache)+'.sig.vertex.mgh'
@@ -99,17 +102,17 @@ class PerformGLM():
                 ocn_mc_f = contrastdir+'mc-z.'+direction+'.th'+str(cache)+'.sig.ocn.mgh'
                 oannot_mc_f = contrastdir+'mc-z.'+direction+'.th'+str(cache)+'.sig.ocn.annot'
                 csdpdf_mc_f = contrastdir+'mc-z.'+direction+'.th'+str(cache)+'.pdf.dat'
-                system('mri_surfcluster --in '+sig_f+' --csd '+csd_mc_f+' --mask '+glmdir+'/mask.mgh --cwsig '+cwsig_mc_f+' --vwsig '+vwsig_mc_f+' --sum '+sum_mc_f+' --ocn '+ocn_mc_f+' --oannot '+oannot_mc_f+' --csdpdf '+csdpdf_mc_f+' --annot aparc --cwpvalthresh 0.05 --surf white')                        				
+                system('mri_surfcluster --in '+sig_f+' --csd '+csd_mc_f+' --mask '+path.join(glmdir,'mask.mgh')+' --cwsig '+cwsig_mc_f+' --vwsig '+vwsig_mc_f+' --sum '+sum_mc_f+' --ocn '+ocn_mc_f+' --oannot '+oannot_mc_f+' --csdpdf '+csdpdf_mc_f+' --annot aparc --cwpvalthresh 0.05 --surf white')                        				
                 if self.check_mcz_summary(sum_mc_f):
                     # self.make_images_results_mc(hemi, analysis_name, contrast_name.replace(contrast_type+'_',''), direction, cwsig_mc_f, oannot_mc_f, str(cache), 1.3)
                     self.cluster_stats_to_file(analysis_name, sum_mc_f, contrast_name.replace(contrast_type+'_',''), direction, explanation)
             else:
-                system('mri_glmfit-sim --glmdir '+glmdir+'/'+contrast_name+' --cache '+str(cache)+' '+direction+' --cwp 0.05 --2spaces')
+                system('mri_glmfit-sim --glmdir '+path.join(glmdir,contrast_name)+' --cache '+str(cache)+' '+direction+' --cwp 0.05 --2spaces')
 #                if self.check_mcz_summary(sum_mc_f):
 #                    self.make_images_results_mc(hemi, analysis_name, contrast_name.replace(contrast_type+'_',''), direction, cwsig_mc_f, oannot_mc_f, str(cache), 1.3)
 
     # def make_images_results_mc(self, hemi, analysis_name, contrast, direction, cwsig_mc_f, oannot_mc_f, cache, thresh):
-    #     self.PATH_save_mc = self.PATH+'results/mc/'
+    #     self.PATH_save_mc = self.PATHglm+'results/mc/'
     #     if not path.isdir(self.PATH_save_mc):
     #         makedirs(self.PATH_save_mc)
     #     fv_cmds = ['-ss '+self.PATH_save_mc+analysis_name+'_'+contrast+'_lat_mc_'+direction+cache+'.tiff -noquit',
@@ -123,7 +126,7 @@ class PerformGLM():
     #     system('freeview -f $SUBJECTS_DIR/fsaverage/surf/'+hemi+'.inflated:overlay='+cwsig_mc_f+':overlay_threshold='+str(thresh)+',5:annot='+oannot_mc_f+' -viewport 3d -layout 1 -cmd fv.cmd')
 
     # def make_images_results_fdr(self, hemi, glmdir, contrast_name, file, thresh):
-    #     self.PATH_save_fdr = self.PATH+'results/fdr/'
+    #     self.PATH_save_fdr = self.PATHglm+'results/fdr/'
     #     if not path.isdir(self.PATH_save_fdr):
     #         makedirs(self.PATH_save_fdr)
 
@@ -139,7 +142,7 @@ class PerformGLM():
     #     system('tksurfer fsaverage '+hemi+' inflated -overlay '+glmdir+'/'+contrast_name+'/'+file+' -fthresh '+str(thresh)+' -tcl scr.tcl')
 
     def cluster_stats_to_file(self, analysis_name, sum_mc_f, contrast_name, direction, explanation):
-        file = path.join(self.PATH,'results','cluster_stats.log')
+        file = path.join(self.PATHglm,'results','cluster_stats.log')
         if not path.isfile(file):
             open(file,'w').close()
         ls = list()
