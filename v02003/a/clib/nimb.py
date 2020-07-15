4f1efc01e81fbded3632702b117440bc82301ac1
Alexandru Hanganu <hanganua@cedar5.cedar.computecanada.ca>
2020-07-15 05:50:44 -0700

    err return

--- v02003/a/clib/nimb.py
+++ v02003/a/clib/nimb.py
@@ -30,18 +30,3 @@ try:
         print(list(filter(None, resp.split(' ')))[-1].strip('\n'))
 except Exception as e:
         print(e)
-        return ''
-
-
-
-'''
-#!/bin/sh
-#SBATCH --account=def-hanganua
-#SBATCH --mem=8G
-#SBATCH --time=03:00:00
-#SBATCH --output=/scratch/$USER/a_tmp/running_output_20200714.out
-
-module load python/3.8.2
-cd /home/$USER/projects/def-hanganua/a/
-python crun.py
-'''
