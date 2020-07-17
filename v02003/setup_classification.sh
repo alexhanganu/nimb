a2f7bb437dd3a909161171ad600232e072e6599e
alexhanganu <alexandru@hanganu.ca>
2020-07-17 11:55:20 -0400

    user asjustments

--- v02003/setup_classification.sh
+++ v02003/setup_classification.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 echo -e "\e[1;31m clear db and status.log in scratch \e[0m"
-mv -v /scratch/hiver85/a_tmp/db /scratch/hiver85/a_tmp/db_bk
-mv -v /scratch/hiver85/a_tmp/status.log /scratch/hiver85/a_tmp/status_bk.log
+mv -v /scratch/$USER/a_tmp/db /scratch/$USER/a_tmp/db_bk
+mv -v /scratch/$USER/a_tmp/status.log /scratch/$USER/a_tmp/status_bk.log
+
+
+#mv -v /scratch/hiver85/a_tmp/db /scratch/hiver85/a_tmp/db_bk
+#mv -v /scratch/hiver85/a_tmp/status.log /scratch/hiver85/a_tmp/status_bk.log
 module load python/3.7.4
 echo -e "\e[1;31m Create json \e[0m"
 python get_MRIs_ppmi_v2.py
