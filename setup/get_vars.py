8ed79b7c828294e0acf0c2c183a1f4af7d50d004
alex hanganu <alexandru@hanganu.ca>
2020-08-14 23:10:08 +0000

    setting up variable in json, done

--- setup/get_vars.py
+++ setup/get_vars.py
@@ -10,41 +10,74 @@ class Get_Vars():
 	def __init__(self):
 
 		if path.exists(path.join(credentials_home, 'projects.json')):
-			f_projects = path.join(credentials_home, 'projects.json')
+			self.projects = self.read_file(path.join(credentials_home, 'projects.json'))
+			self.d_all_vars = self.get_vars(self.projects)
 		else:
-			f_projects = path.join(path.dirname(path.abspath(__file__)), 'projects.json')
-			print('PROJECTS AND VARIABLES ARE NOT DEFINED. this can be done in the files located at: '+credentials_home)
-			for file in ['local.json', 'projects.json', 'remote1.json']:
-				shutil.copy(path.join(path.dirname(path.abspath(__file__)), file), path.join(credentials_home, file))
-		self.projects = self.get_vars(f_projects)
-		self.d_all_vars = dict()
-		for location in self.projects['LOCATION']:
-			try:
-				self.d_all_vars[location] = self.get_vars(path.join(path.dirname(path.abspath(__file__)), location+'.json'))
-			except Exception as e:
-				print(e)
-		if 'local' in self.d_all_vars and len(self.d_all_vars['local']['USER']["users_list"]) > 1:
-				self.change_username()
+			shutil.copy(path.join(path.dirname(path.abspath(__file__)), 'projects.json'), path.join(credentials_home, 'projects.json'))
+			shutil.copy(path.join(path.dirname(path.abspath(__file__)), 'remote1.json'), path.join(credentials_home, 'remote1.json'))
+			self.projects = self.read_file(path.join(path.dirname(path.abspath(__file__)), 'projects.json'))
+			self.d_all_vars = self.get_default_vars(self.projects)
 
-	def get_vars(self, file):
+
+	def read_file(self, file):
 		with open(file) as jf:
 			return json.load(jf)
 
-	def verify_local_user(self):
+
+	def get_vars(self, projects):
+		d_all_vars = dict()
+                for location in projects['LOCATION']:
+                        try:
+                                d_all_vars[location] = self.read_file(path.join(path.dirname(path.abspath(__file__)), location+'.json'))
+                        except Exception as e:
+                                print(e)
+                d_all_vars = self.change_username(d_all_vars)
+		return d_all_vars
+
+	def get_default_vars(self, projects):
+		d_all_vars = self.get_vars(projects)
+		d_all_vars['local'] = self.set_local_nimb(d_all_vars['local'], projects['PROJECTS'][0])
+		self.save_json('local.json', d_all_vars['local'], credentials_home)
+		print('PROJECTS AND VARIABLES ARE NOT DEFINED. this can be done in the files located at: '+credentials_home)
+		return d_all_vars
+
+	def verify_local_user(self, user):
 		from .get_username import _get_username
-		user = self.d_all_vars['local']['USER']['user']
 		user_local = _get_username()
 		if user_local != user:
-			return True, user, user_local
+			return True, user_local
 
-	def change_username(self):
-		change, user, user_local = self.verify_local_user()
-		if change:
-			print('changing username')
-			self.d_all_vars['local']['USER']['user'] = user_local
-			for variable in self.d_all_vars['local']['NIMB_PATHS']:
-				self.d_all_vars['local']['NIMB_PATHS'][variable] = self.d_all_vars['local']['NIMB_PATHS'][variable].replace(user, user_local)
-			self.d_all_vars['local']['FREESURFER']["FREESURFER_HOME"] = self.d_all_vars['local']['FREESURFER']["FREESURFER_HOME"].replace(user, user_local)
-			self.d_all_vars['local']['FREESURFER']["FS_SUBJECTS_DIR"] = self.d_all_vars['local']['FREESURFER']["FS_SUBJECTS_DIR"].replace(user, user_local)
-			self.d_all_vars['local']['FREESURFER']["export_FreeSurfer_cmd"] = self.d_all_vars['local']['FREESURFER']["export_FreeSurfer_cmd"].replace(user, user_local)
+	def change_username(self, data):
+		if 'local' in data:
+			user = data['local']['USER']['user']
+			change,user_local = self.verify_local_user(user)
+			if change:
+				print('changing username')
+				data['local']['USER']['user'] = user_local
+				for variable in data['local']['NIMB_PATHS']:
+					data['local']['NIMB_PATHS'][variable] = data['local']['NIMB_PATHS'][variable].replace(user, user_local)
+				data['local']['FREESURFER']["FREESURFER_HOME"] = data['local']['FREESURFER']["FREESURFER_HOME"].replace(user, user_local)
+				data['local']['FREESURFER']["FS_SUBJECTS_DIR"] = data['local']['FREESURFER']["FS_SUBJECTS_DIR"].replace(user, user_local)
+				data['local']['FREESURFER']["export_FreeSurfer_cmd"] = data['local']['FREESURFER']["export_FreeSurfer_cmd"].replace(user, user_local)
+		return data
 
+	def save_json(self, file, data, dst):
+		with open(path.join(dst, file), 'w') as jf:
+			json.dump(data, jf, indent=4)
+
+	def set_local_nimb(self, data, project):
+		NIMB_HOME = path.abspath(path.join(path.dirname(__file__), '..'))
+		print(NIMB_HOME)
+		data['NIMB_PATHS']['NIMB_HOME']               = NIMB_HOME
+		data['NIMB_PATHS']['NIMB_tmp']                = path.join(NIMB_HOME, 'tmp')
+		data['NIMB_PATHS']['NIMB_NEW_SUBJECTS']       = path.join(NIMB_HOME, 'tmp', 'nimb_new_subjects')
+		data['NIMB_PATHS']['NIMB_PROCESSED_FS']       = path.join(NIMB_HOME, 'tmp', 'nimb_processed_fs')
+		data['NIMB_PATHS']['NIMB_PROCESSED_FS_error'] = path.join(NIMB_HOME, 'tmp', 'nimb_processed_fs_error')
+		data['NIMB_PATHS']['miniconda_home']          = path.join(NIMB_HOME, 'miniconda3')
+		data['NIMB_PATHS']['miniconda_python_run']    = path.join(NIMB_HOME, 'miniconda3','bin','python3.7').replace(path.expanduser("~"),"~")
+		data['FREESURFER']['FREESURFER_HOME']         = path.join(NIMB_HOME, 'freesurfer')
+		data['FREESURFER']['FS_SUBJECTS_DIR']         = path.join(NIMB_HOME, 'freesurfer', 'subjects')
+		data['FREESURFER']['export_FreeSurfer_cmd']   = "export FREESURFER_HOME="+path.join(NIMB_HOME, 'freesurfer')
+		data['FREESURFER']['GLM_dir']                 = path.join(NIMB_HOME, project, 'glm')
+		data['STATS_PATHS']['STATS_HOME']             = path.join(NIMB_HOME, project, 'stats')
+		return data
