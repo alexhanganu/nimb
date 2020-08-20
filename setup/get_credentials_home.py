686212499fa83cb8bafed63589af5abe1360206a
alex hanganu <alexandru@hanganu.ca>
2020-08-20 16:58:31 -0400

    adjusting to win32

--- setup/get_credentials_home.py
+++ setup/get_credentials_home.py
@@ -1,13 +1,18 @@
-from os import path, makedirs
-from .get_username import _get_username
+from os import path, makedirs, environ
+from sys import platform
 
 def _get_credentials_home():
+    if platform == 'win32':
+        home = environ['HOMEPATH']
+    else:
+        home = environ['HOME']
     try:
         from credentials_path import credentials_home
-        credentials_home = credentials_home.replace("~",path.join("/home", _get_username()))
+        credentials_home = credentials_home.replace("~", home)
     except Exception as e:
-        credentials_home = str(open("credentials_path").readlines()[0]).replace("~",path.join("/home", _get_username()))
+        credentials_home = str(open("credentials_path").readlines()[0]).replace("~",home)
     if not path.exists(credentials_home):
         makedirs(credentials_home)
     return credentials_home
+
 
