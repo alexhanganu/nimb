9582ec59fe366f266cce82ee5beb5ad917f0ea99
alex hanganu <alexandru@hanganu.ca>
2020-09-01 17:07:06 -0400

    stats

--- stats/linear_regression_moderation.py
+++ stats/linear_regression_moderation.py
@@ -1,14 +1,12 @@
 # !/usr/bin/env python
 # coding: utf-8
-# last update: 2020-04-06
-
-# logistic regression on 2 variables
-# also called moderation analysis
-
-# Lynn Valeyry Verty, Alex Hanganu.
+# last update: 2020-09-01
+# linear regression on 2 variables (moderation analysis)
+# Lynn Valeyry Verty, Alex Hanganu
 
 from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
+from stats import db_processing
 
 
 def get_main_dict(group_param, regression_param):
@@ -39,3 +37,12 @@ def compute_linreg_data(df, ls_cols_X, group_param, regression_param):
         d1['B_'+regression_param].append((reg.coef_[0])[2])
         d1['Intercept'].append(reg.intercept_[0])
     return d1
+
+def linreg_moderation_results(df_X_linreg, ls_cols_X_atlas, group_param, regression_param, path_dir_save_results, atlas):
+    d_result = compute_linreg_data(df_X_linreg,
+                                   ls_cols_X_atlas,
+                                   group_param,
+                                   regression_param)
+    df_result = db_processing.create_df_from_dict(d_result)
+    db_processing.save_df(df_result, path.join(path_dir_save_results,'linreg_moderation_'+atlas+'_all.xlsx'), sheet_name='linreg_moderation')
+
--- stats/stats_LogisticRegression.py
+++ stats/stats_LogisticRegression.py
@@ -1,17 +1,17 @@
 #!/bin/python
+from os import path
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import warnings; warnings.filterwarnings("ignore")
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score
 
 
 class Logistic_Regression():
         
-    def __init__(self, X_scaled, y_labeled, group_col, f_results, Log_ROC):
+    def __init__(self, X_scaled, y_labeled, group_col, path_results):
 
         X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_labeled, test_size=0.3, random_state=0)
 
@@ -24,6 +24,27 @@ class Logistic_Regression():
 
         matrix = confusion_matrix(y_test, y_pred)
         print('\nConfusion Matrix:\n',str(matrix))
+        with open(path.join(path_results, 'confusion_matrix.txt'),'w') as f:
+            f.write('\nConfusion Matrix:\n')
+            f.write(str(matrix)+'\n')
 
         print(classification_report(y_test, y_pred))
+        with open(path.join(path_results, 'classification_report.txt'),'w') as f:
+            f.write('\nClassification Report:\n')
+            f.write(classification_report(y_test, y_pred)+'\n')
 
+        # ROC Curve
+        logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+        fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+        plt.figure()
+        plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+        plt.plot([0, 1], [0, 1],'r--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
+        plt.savefig(path.join(path_results, 'Log_ROC.png'))
+#        plt.show()
+        print('Logistic Regression DONE')
