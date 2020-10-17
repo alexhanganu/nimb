3244769664d0f0cd0b449265c00c068ba3134ef9
alexhanganu <alexandru@hanganu.ca>
2020-10-17 10:19:20 -0400

    classify_bids - class finished, must be checked

--- classification/classify_bids.py
+++ classification/classify_bids.py
@@ -1,5 +1,10 @@
 #!/bin/python
-# Kim Phuong Pham 20200628;  Alexandru Hanganu 20201015
+"""
+authors:
+Alexandru Hanganu
+Kim Phuong Pham
+"""
+
 '''
 1) read the folder with subjects
 3) extract paths for the anat MRIs
@@ -33,7 +38,31 @@ class MakeBIDS_subj2process():
             self.d_subjects[subject] = {}
             path_2mris = self._get_MR_paths(path.join(self.DIR_SUBJECTS, subject))
             ls_MR_paths = self.exclude_MR_types(path_2mris)
-            print("ls_MR_paths: ", ls_MR_paths)
+#            print("ls_MR_paths: ", ls_MR_paths)
+            ls_sessions, d_paths = self.get_ls_sessions(ls_MR_paths)
+#            print(ls_sessions)
+            d_sessions = self.classify_by_sessions(ls_sessions)
+#            print(d_sessions)
+            dict_sessions_paths = self.make_dict_sessions_with_paths(d_paths, d_sessions)
+            d_ses_MR_types = self.classify_by_MR_types(dict_sessions_paths)
+            d_BIDS_structure = self.make_BIDS_structure(d_ses_MR_types)
+#            print(d_BIDS_structure)
+            d_subjects[subject] = d_BIDS_structure
+            self.save_json(DIR_SUBJECTS, "all_subjects", d_subjects)
+        print("classification of new subjects is complete")
+        if multiple_T1_entries == 1:
+            from get_mr_params import verify_MRIs_for_similarity
+            d_subjects = verify_MRIs_for_similarity(d_subjects, NIMB_HOME, NIMB_tmp, flair_t2_add)
+        else:
+            d_subjects = self.keep_only1_T1(d_subjects)
+
+        f_new_subjects = path.join(NIMB_tmp,'new_subjects.json')
+        self.save_json(NIMB_tmp, f_new_subjects, d_subjects)
+        if path.exists(path.join(NIMB_tmp, f_new_subjects)):
+            return True
+        else:
+            return False
+
 
     def _get_MR_paths(self, path2subj):
         if '.zip' in path2subj:
@@ -91,8 +120,152 @@ class MakeBIDS_subj2process():
                     ls.remove(mr_path)
                     break
         return ls
-    
 
+    def get_ls_sessions(self, ls):
+        # add types
+        mr_types = {'t1': ['t1', 'spgr', 'rage', ],
+                    'flair': ['flair', ],
+                    't2': ['t2', ],
+                    'dwi': ('hardi', 'dti', 'diffus',),
+                    'rsfmri': ['resting_state_fmri', 'rsfmri', ],
+                    'fieldmap': ['field_map', 'field_mapping', 'fieldmap', ]}
+        d_paths = defaultdict(list)
+        ls_sessions = list()
+
+        for mr_path in ls:
+            # add date to sessions
+            for date in mr_path.split('/')[2:]:
+                if validate_if_date(date):
+                    if date not in ls_sessions:
+                        ls_sessions.append(date)
+                    break
+            # add paths by date and type
+            for mr_name_ls in mr_types.values():
+                for mr_name in mr_name_ls:
+                    if mr_name.lower() in mr_path.lower():
+                        d_paths[date].append(mr_path)
+        return ls_sessions, d_paths
+    
+    def classify_by_sessions(self, ls):
+        d = {}
+        oneday = dt.timedelta(days=1)
+        n = 1
+        ses_name = 'ses-'+str(n).zfill(2)
+        d[ses_name] = list()
+        for ses in sorted(ls):
+            if len(d[ses_name])<1:
+                d[ses_name].append(ses)
+            else:
+                date_new = dt.datetime.strptime(ses, '%Y-%m-%d_%H_%M_%S.%f')
+                date_before = dt.datetime.strptime(d[ses_name][0], '%Y-%m-%d_%H_%M_%S.%f')
+                if date_new-date_before < oneday:
+                    d[ses_name].append(ses)
+                else:
+                    n +=1
+                    ses_name = 'ses-'+str(n).zfill(2)
+                    d[ses_name] = list()
+                    d[ses_name].append(ses)
+        return d
+
+
+
+    def make_dict_sessions_with_paths(self, d_paths, d_sessions):
+        d_ses_paths = {}
+
+        for ses in d_sessions:
+            d_ses_paths[ses] = list()
+            for date in d_sessions[ses]:
+                for path in d_paths[date]:
+                    if path not in d_ses_paths[ses]:
+                        d_ses_paths[ses].append(path)
+        return d_ses_paths
+
+
+
+    def classify_by_MR_types(self, dict_sessions_paths):
+        d_ses_MR_types = {}
+        for ses in dict_sessions_paths:
+            d_ses_MR_types[ses] = {}
+            for mr_path in dict_sessions_paths[ses]:
+                mr_type = get_MR_types(mr_path)
+                if mr_type != 'none':
+                    if mr_type not in d_ses_MR_types[ses]:
+                        d_ses_MR_types[ses][mr_type] = list()
+                    d_ses_MR_types[ses][mr_type].append(mr_path)
+                else:
+                    print(mr_type,mr_path,'none')
+        return d_ses_MR_types
+
+
+    def subjects_less_f(self, limit, ls_all_raw_subjects):
+        ls_subjects = list()
+        for folder in ls_all_raw_subjects:
+            if len([f for f in listdir(SUBJECTS_DIR_RAW+folder)])<limit:
+                ls_subjects.append(folder)
+
+        return ls_subjects
+
+    def subjects_nodcm(self, ls_all_raw_subjects):
+        ls_subjects = list()
+        for folder in ls_all_raw_subjects:
+            for file in listdir(SUBJECTS_DIR_RAW+folder):
+                if not file.endswith('.dcm'):
+                    ls_subjects.append(folder)
+                    break
+        return ls_subjects
+
+    def subj_no_t1(self, ls_all_raw_subjects):
+        ls_subjects = list()
+        for folder in ls_all_raw_subjects:
+            if '_flair' in folder:
+                    if folder.replace('_flair','_t1') in ls_all_raw_subjects:
+                        pass
+                    else:
+                        ls_subjects.append(folder)
+            if '_t2' in folder:
+                    if folder.replace('_t2','_t1') in ls_all_raw_subjects:
+                        pass
+                    else:
+                        ls_subjects.append(folder)
+        return ls_subjects
+
+
+    def make_BIDS_structure(self, d_ses_MR_types):
+        BIDS_groups = {'anat':['t1','flair','t2'],'dwi':['dwi','bval','bvec'],'func':['rsfmri','fieldmap',]}
+        d_BIDS_structure = {}
+        for ses in d_ses_MR_types:
+            d_BIDS_structure[ses] = {}
+            for key in d_ses_MR_types[ses]:
+                for group in BIDS_groups:
+                    if key in BIDS_groups[group]:
+                        if group not in d_BIDS_structure[ses]:
+                            d_BIDS_structure[ses][group] = {}
+                        d_BIDS_structure[ses][group][key] = d_ses_MR_types[ses][key]
+                        break
+        return d_BIDS_structure
+
+
+    def keep_only1_T1(self, d_subjects):
+        for subject in d_subjects:
+            for session in d_subjects[subject]:
+                if 'anat' in d_subjects[subject][session] and 't1' in d_subjects[subject][session]['anat']:
+                    d_subjects[subject][session]['anat']['t1'] = d_subjects[subject][session]['anat']['t1'][:1]
+                    if 'flair' in d_subjects[subject][session]['anat']:
+                        d_subjects[subject][session]['anat'].pop('flair', None)
+                    if 't2' in d_subjects[subject][session]['anat']:
+                        d_subjects[subject][session]['anat'].pop('t2', None)
+        return d_subjects
+
+
+    def save_json(self, NIMB_tmp, file, dictionary):
+        with open(path.join(NIMB_tmp, file),'w') as f:
+            json.dump(dictionary, f, indent=4)
+
+# ===============================================================
+# ===============================================================
+# BELOW can be removed after the class is confirmed to be working
+# ===============================================================
+# ===============================================================
 
 
 def get_paths2dcm_files(path_root):
@@ -307,10 +480,6 @@ def get_dict_MR_files2process(DIR_SUBJECTS, NIMB_HOME, NIMB_tmp, multiple_T1_ent
     print("classification of new subjects is running ...")
     d_subjects = dict()
     for subject in listdir(DIR_SUBJECTS):
-        if '.zip' in subject:
-            content = chk_if_archive(subject)
-            print(content)
-
         d_subjects[subject] = {}
         ls_MR_paths = exclude_MR_types(get_paths2dcm_files(path.join(DIR_SUBJECTS,subject)))
         #print("ls_MR_paths: ", ls_MR_paths)
