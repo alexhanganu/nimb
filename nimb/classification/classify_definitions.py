744e6fc0754710b6f5c56d78bae092e4c971476c
alexhanganu <alexandru@hanganu.ca>
2020-11-08 09:08:45 -0500

    classify_definitions adjusted

--- nimb/classification/classify_definitions.py
+++ nimb/classification/classify_definitions.py
@@ -1,16 +1,22 @@
 #!/bin/python
 
-mr_types_2exclude = ['calibration','localizer','loc','moco','perfusion','tse',
-                    'survey','scout','hippo','cbf','isotropic','fractional',
-                    'pasl','multi_reset','dual_echo','gre','average_dc']
+mr_types_2exclude = ['calibration','localizer','loc',
+                     'cbf','isotropic','fractional','average_dc',
+                     'perfusion','tse','survey','scout','hippo',
+                     'pasl','multi_reset','dual_echo','gre',]
 
-mr_modalities = {'flair'  :['flair',],
-                'dwi'     :['hardi','dti','diffus'],
-                'rsfmri'  :['resting_state_fmri','rsfmri'],
-                'fieldmap':['field_map','field_mapping','fieldmap'],
-                't1'      :['t1','spgr','rage'],
-                't2'      :['t2',],}
+mr_modalities = {'flair' :['flair',],
+                 'dwi'   :['hardi','dti','diffus'],
+                 'bold'  :['resting_state_fmri','rsfmri'],
+                 'fmap'  :['field_map','field_mapping','fieldmap'],
+                 't1'    :['t1','spgr','rage'],
+                 't2'    :['t2',],
+                 'pd'    :['pd',],
+                 'swi'   :['swi',]
+                }
 
-BIDS_types = {'anat':['t1','flair','t2'],
-               'dwi':['dwi','bval','bvec'],
-               'func':['rsfmri','fieldmap',]}
+BIDS_types = {'anat':['t1','flair','t2', 'swi', 'pd'],
+              'dwi' :['dwi','bval','bvec'],
+              'func':['bold','rsfmri'],
+              'fmap':['fmap',]
+                }
