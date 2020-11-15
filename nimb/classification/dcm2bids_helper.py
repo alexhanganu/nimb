3ad3b2c7294a87c029881099e8a098bdfe82ecf2
kim phuong <pham@kimphuong.ca>
2020-11-15 18:03:03 -0500

    Fix_error dcm2bids_helper.py

--- nimb/classification/dcm2bids_helper.py
+++ nimb/classification/dcm2bids_helper.py
@@ -13,7 +13,7 @@ from os import path, system, makedirs, listdir
 import shutil
 import json
 
-from classify_definitions import BIDS_types, mr_modalities
+from classification.classify_definitions import BIDS_types, mr_modalities
 
 class DCM2BIDS_helper():
     """
@@ -40,14 +40,20 @@ class DCM2BIDS_helper():
             self.config_file = self.get_config_file()
             self.SUBJ_NAME = self.get_sub()
             print(self.SUBJ_NAME)
+            # Run the dcm2bids app
             system('dcm2bids -d {} -p {} -c {} -o {}'.format(self.DICOM_DIR, self.SUBJ_NAME, self.config_file, self.OUTPUT_DIR))
+            # set the subject dir
             self.sub_SUBJDIR = path.join(self.OUTPUT_DIR, 'tmp_dcm2bids', 'sub-{}'.format(self.SUBJ_NAME))
             self.chk_if_processed()
 
+
     def get_sub(self):
+        """Get list of all file names in the input dir """
         return listdir(self.DICOM_DIR)[0]
 
+
     def get_config_file(self):
+        """Copy from... to...?"""
         config_file = path.join(self.OUTPUT_DIR,
                              'dcm2bids_config_{}.json'.format(self.project))
         if path.exists(config_file):
@@ -57,7 +63,12 @@ class DCM2BIDS_helper():
                         config_file)
             return config_file
 
+
     def chk_if_processed(self):
+        """Check if any unconverted,
+          - if not converted, try to create the config file (get_sidecar(), update_config())
+          - redo run() up to repeat_lim
+        """
         if [i for i in listdir(self.sub_SUBJDIR) if '.nii.gz' in i]:
             if self.repeat_updating < self.repeat_lim:
                 self.get_sidecar()
@@ -69,61 +80,78 @@ class DCM2BIDS_helper():
         else:
             self.rm_dir(self.sub_SUBJDIR)
 
+
     def get_sidecar(self):
+        """...."""
+        print ("get_sidecar_sub_dir", self.sub_SUBJDIR)
         sidecar = [i for i in listdir(self.sub_SUBJDIR) if '.json' in i][0]
         self.sidecar_content = self.get_json_content(path.join(self.sub_SUBJDIR, sidecar))
         data_Type, modality, criterion = self.classify_mri()
         self.update_config(data_Type, modality, criterion)
 
+
     def update_config(self, data_Type, modality, criterion):
+        """....."""
+        print("update_config")
         self.config = self.get_json_content(self.config_file)
         if self.chk_if_in_config(data_Type, modality, criterion):
             new_des = {
-               ['dataType' : data_Type, 'modalityLabel' : modality,
-               'criteria':{criterion: self.sidecar_content[criterion]}]}
+               'dataType': data_Type,
+               'modalityLabel' : modality,
+               'criteria':{criterion: self.sidecar_content[criterion]}}
             self.config['descriptions'].append(new_des)
             self.save_json(self.config, self.config_file)
         else:
            print('criterion {} present in config file'.format(criterion))
 
+
     def chk_if_in_config(self, data_Type, modality, criterion):
+        """...."""
         for des in self.config['descriptions']:
-            if data_Type in self.config['descriptions'][des]['data_Type'] and
-                modality in self.config['descriptions'][des]['modalityLabel'] and 
-                self.sidecar_content[criterion] in self.config['descriptions'][des]['criteria'][criterion]:
-                return True
+            if data_Type in self.config['descriptions'][des]['data_Type'] and \
+               modality in self.config['descriptions'][des]['modalityLabel'] and \
+               self.sidecar_content[criterion] in self.config['descriptions'][des]['criteria'][criterion]:
+                    return True
             else:
                 self.run == '0'
                 return False
 
+
     def run_helper(self):
+        """...."""
         helper_dir = path.join(self.OUTPUT_DIR, 'tmp_dcm2bids', 'helper')
         system('dcm2bids_helper -d {} -o {}'.format(self.DICOM_DIR, self.OUTPUT_DIR))
         # read the .json file and add parameters in the config file
         self.sidecar_content = open(path.join(helper_dir,
                       [i for i in listdir(path.join()) if '.json' in i][0]), 'r').readlines()
-        return sidecar
+        return self.sidecar_content
+
 
     def classify_mri(self):
+        """...."""
         # BIDS_types
         criterion = 'SeriesDescription'
         type = 'anat'
         modality = 'T1w'
         return type, modality, criterion
 
+
     def validate_bids(self):
         # https://github.com/bids-standard/bids-validator
         return True
 
+
     def save_json(self, data, file):
         with open(file, 'w') as f:
             json.dump(data, f, indent = 4)
-    
+
+
     def get_json_content(self, file):
         with open(file, 'r') as f:
             return json.load(f)
 
     def get_SUBJ_DIR(self):
+        """Get the path of DICOM_DIR"""
         DICOM_DIR = self.proj_vars['SOURCE_SUBJECTS_DIR'][1]
         if path.exists(DICOM_DIR):
             return DICOM_DIR
@@ -131,10 +159,13 @@ class DCM2BIDS_helper():
             print('    path is invalid: {}'.format(DICOM_DIR))
             return 'PATH_IS_MISSING'
 
+
     def rm_dir(self, DIR):
         system('rm -r {}'.format(DIR))
 
+
     def chk_dir(self):
+        """Check if a directory exists. If not, create a directory"""
         OUTPUT_DIR = self.proj_vars['SOURCE_BIDS_DIR'][1]
         if not path.exists(OUTPUT_DIR):
             makedirs(OUTPUT_DIR)
