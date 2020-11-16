30ab3123515d311b72ef15516162b262f869be93
kim phuong <pham@kimphuong.ca>
2020-11-15 22:34:12 -0500

    Add exception

--- nimb/classification/dcm2bids_helper.py
+++ nimb/classification/dcm2bids_helper.py
@@ -26,7 +26,6 @@ class DCM2BIDS_helper():
     """
 
     def __init__(self, proj_vars, project, repeat_lim = 1):
-        print("test_Init DCM2BIDS")
         self.proj_vars  = proj_vars
         self.project    = project    #project item in projects.json
         self.run_stt        = 1
@@ -34,36 +33,47 @@ class DCM2BIDS_helper():
         self.repeat_updating = 0
         self.DICOM_DIR  = self.get_SUBJ_DIR()
         self.OUTPUT_DIR = self.chk_dir()
-        print("test_Init DCM2BIDS_helper end")
 
 
     def run(self, subjid = 'none'):
-        print("test run_stt", self.run_stt)
         #run dcm2bids:
         if self.run_stt == 1:
             self.config_file = self.get_config_file()
             print("config_file", self.config_file)
-            for subj_name in self.get_sub():
-                self.SUBJ_NAME = subj_name
-                print(self.SUBJ_NAME)
-                # Run the dcm2bids app
-                system('dcm2bids -d {} -p {} -c {} -o {}'.format(self.DICOM_DIR, self.SUBJ_NAME, self.config_file, self.OUTPUT_DIR))
-                # set the subject dir
-                self.sub_SUBJDIR = path.join(self.OUTPUT_DIR, 'tmp_dcm2bids', 'sub-{}'.format(self.SUBJ_NAME))
-                self.chk_if_processed()
+            list_subj = self.get_sub()
+            if list_subj != None:
+                for subj_name in list_subj:
+                    self.SUBJ_NAME = subj_name
+                    print(self.SUBJ_NAME)
+                    # Run the dcm2bids app
+                    try:
+                        system('dcm2bids -d {} -p {} -c {} -o {}'.format(self.DICOM_DIR, self.SUBJ_NAME, self.config_file, self.OUTPUT_DIR))
+                    except Exception as e:
+                        print(e)
+                    # set the subject dir
+                    self.sub_SUBJDIR = path.join(self.OUTPUT_DIR, 'tmp_dcm2bids', 'sub-{}'.format(self.SUBJ_NAME))
+                    self.chk_if_processed()
+            else:
+                return
+
 
 
     def get_sub(self):
         """Get list of all file names in the input dir """
-        print("get_sub", listdir(self.DICOM_DIR))
-        return listdir(self.DICOM_DIR)
+        try:
+            list_files = listdir(self.DICOM_DIR)[0]
+            return list_files
+        except Exception as e:
+            print(e)
+            return
 
 
     def get_config_file(self):
-        """Copy from... to...?"""
+        """Get the dcm2bids_config_{project_name}.json.
+           If not exist, get dcm2bids_config_default.json
+        """
         config_file = path.join(self.OUTPUT_DIR,
                              'dcm2bids_config_{}.json'.format(self.project))
-        print("Config file:", config_file)
         if path.exists(config_file):
             return config_file
         else:
@@ -77,7 +87,6 @@ class DCM2BIDS_helper():
           - if not converted, try to create the config file (get_sidecar(), update_config())
           - redo run() up to repeat_lim
         """
-        print("test chk_if_processed")
         if [i for i in listdir(self.sub_SUBJDIR) if '.nii.gz' in i]:
             if self.repeat_updating < self.repeat_lim:
                 self.get_sidecar()
@@ -92,18 +101,14 @@ class DCM2BIDS_helper():
 
     def get_sidecar(self):
         """...."""
-        print ("get_sidecar_sub_dir", self.sub_SUBJDIR)
         sidecar = [i for i in listdir(self.sub_SUBJDIR) if '.json' in i][0]
         self.sidecar_content = self.get_json_content(path.join(self.sub_SUBJDIR, sidecar))
-        print("get_sidecar content:", self.sidecar_content)
         data_Type, modality, criterion = self.classify_mri()
-        print("get_sidecar:", data_Type, modality, criterion)
         self.update_config(data_Type, modality, criterion)
 
 
     def update_config(self, data_Type, modality, criterion):
         """....."""
-        print("update_config")
         self.config = self.get_json_content(self.config_file)
         if self.chk_if_in_config(data_Type, modality, criterion):
             new_des = {
@@ -118,9 +123,8 @@ class DCM2BIDS_helper():
 
     def chk_if_in_config(self, data_Type, modality, criterion):
         """...."""
-        print ("test chk_if_in_config")
+        print ("chk_if_in_config")
         for des in self.config['descriptions']:
-            print("json:",des)
             # print("json:", des['data_Type'])
             # print("json2:", des['modalityLabel'])
             if data_Type in des['dataType'] and \
@@ -134,7 +138,6 @@ class DCM2BIDS_helper():
 
     def run_helper(self):
         """...."""
-        print("test run_helper")
         helper_dir = path.join(self.OUTPUT_DIR, 'tmp_dcm2bids', 'helper')
         system('dcm2bids_helper -d {} -o {}'.format(self.DICOM_DIR, self.OUTPUT_DIR))
         # read the .json file and add parameters in the config file
@@ -146,7 +149,6 @@ class DCM2BIDS_helper():
     def classify_mri(self):
         """...."""
         # BIDS_types
-        print("test classify_mri")
         criterion = 'SeriesDescription'
         type = 'anat'
         modality = 'T1w'
@@ -160,20 +162,16 @@ class DCM2BIDS_helper():
 
 
     def save_json(self, data, file):
-        print("test save_json")
         with open(file, 'w') as f:
             json.dump(data, f, indent = 4)
 
 
     def get_json_content(self, file):
-        print("test get_json_content")
         with open(file, 'r') as f:
             return json.load(f)
 
     def get_SUBJ_DIR(self):
         """Get the path of DICOM_DIR"""
-        print("test get_SUBJ_DIR")
-
         DICOM_DIR = self.proj_vars['SOURCE_SUBJECTS_DIR'][1]
         if path.exists(DICOM_DIR):
             return DICOM_DIR
@@ -188,7 +186,6 @@ class DCM2BIDS_helper():
 
     def chk_dir(self):
         """Check if a directory exists. If not, create a directory"""
-        print("test chk_dir")
         OUTPUT_DIR = self.proj_vars['SOURCE_BIDS_DIR'][1]
         if not path.exists(OUTPUT_DIR):
             makedirs(OUTPUT_DIR)
