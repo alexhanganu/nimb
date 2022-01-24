3880f49797d7c32504914212394933943b8aa3b1
alex hanganu <alexandru.hanganu@umontreal.ca>
2022-01-24 07:09:52 -0800

    errs adjs in freesurfer pipeline

--- nimb/processing/freesurfer/fs_glm_extract_images.py
+++ nimb/processing/freesurfer/fs_glm_extract_images.py
@@ -24,6 +24,8 @@ class SaveGLMimages():
         self.PATHglm_glm       = self.param.PATHglm_glm
         self.hemispheres       = hemi
         self.mcz_sim_direction = self.param.mcz_sim_direction
+        self.measures          = self.vars_fs["GLM_measurements"]
+        self.threshs           = self.vars_fs["GLM_thresholds"]
         self.f_with_cmds       = path.join(self.PATHglm, 'fv.cmd')
 
         self.fdr_thresh        = 3.0 #p = 0.001
@@ -34,17 +36,17 @@ class SaveGLMimages():
 
     def run(self):
         if path.exists(self.param.sig_fdr_json):
-            print('reading images for FDR significant results')
+            print('    reading images for FDR significant results')
             self.read_fdr_images()
         else:
             print('    folder with glm results is missing at:', self.param.sig_fdr_json)
         if path.exists(self.param.sig_mc_json):
-            print('reding images with MC-z corrected results')
+            print('    reading images with MC-z corrected results')
             self.read_mc_images()
         else:
             print('    folder with glm results is missing at:', self.param.sig_mc_json)
         # elif path.exists(self.param.files_for_glm):
-        #     print('reading images after FreeSurfer GLM analysis')
+        #     print('    reading images after FreeSurfer GLM analysis')
         #     self.read_glm_subdirs()
 
         # cleaning unnecessary files:
@@ -59,20 +61,24 @@ class SaveGLMimages():
         cluster_stats      = path.join(self.param.PATHglm_results,'cluster_stats.log')
         cluster_stats_2csv = path.join(self.param.PATHglm_results,'cluster_stats.csv')
         if path.exists(cluster_stats) and not path.exists(cluster_stats_2csv):
-            print(f'\ntransforming file {cluster_stats} to file: {cluster_stats_2csv}')
+            print(f'\n    transforming file {cluster_stats} to file: {cluster_stats_2csv}')
             from fs_glm_runglm import ClusterFile2CSV
             ClusterFile2CSV(cluster_stats, cluster_stats_2csv)
 
 
     def read_fdr_images(self):
         img = load_json(self.param.sig_fdr_json)
+        ls_img = list(img.keys())
         for sig in img:
             analysis_name = img[sig]['analysis_name']
             glmdir = path.join(self.param.PATH_img, analysis_name)
             self.make_images_results_fdr(img[sig]['hemi'], glmdir, analysis_name, img[sig]['fsgd_type_contrast'])
+            print(f"    \n\n\n{len(ls_img[ls_img.index(sig):])} images LEFT for extraction")
+
 
     def read_mc_images(self):
         img = load_json(self.param.sig_mc_json)
+        ls_img = list(img.keys())
         for sig in img:
             analysis_name      = img[sig]['analysis_name']
             contrast           = img[sig]['contrast']
@@ -84,46 +90,52 @@ class SaveGLMimages():
                                         img[sig]['direction'],
                                         cwsig_mc_f,
                                         oannot_mc_f)
-    def read_glm_subdirs(self):
-        files_glm = load_json(self.param.files_for_glm)
-        for fsgd_type in files_glm:
-            for fsgd_file in files_glm[fsgd_type]['fsgd']:
-                fsgd_f_unix = path.join(self.PATHglm, 'fsgd_unix', '{}_unix.fsgd'.format(fsgd_file.replace('.fsgd','')))
-                for hemi in hemispheres:
-                    for meas in self.vars_fs["GLM_measurements"]:
-                        mcz_meas = self.param.GLM_MCz_meas_codes[meas]
-                        for thresh in self.vars_fs["GLM_thresholds"]:
-                            analysis_name = '{}.{}.{}.fwhm{}'.format(fsgd_file.replace('.fsgd',''), meas, hemi, str(thresh))
-                            glmdir = path.join(self.PATHglm_glm, analysis_name)
-                            for contrast_file in files_glm[fsgd_type]['mtx']:
-                                fsgd_type_contrast = contrast_file.replace('.mtx','')
-                                contrast = fsgd_type_contrast.replace(fsgd_type+'_','')
-                                path_2contrast = path.join(glmdir, fsgd_type_contrast)
-                                if self.check_maxvox(glmdir, fsgd_type_contrast):
-                                    self.make_images_results_fdr(hemi, glmdir, analysis_name, fsgd_type_contrast)
-                                for direction in self.mcz_sim_direction:
-                                    mcz_header  = 'mc-z.{}.{}{}'.format(direction, mcz_meas, str(self.mc_cache_thresh))
-                                    sum_mc_f    = path.join(path_2contrast,'{}.sig.cluster.summary'.format(mcz_header))
-                                    cwsig_mc_f  = path.join(path_2contrast,'{}.sig.cluster.mgh'.format(mcz_header))
-                                    oannot_mc_f = path.join(path_2contrast,'{}.sig.ocn.annot'.format(mcz_header))
-                                    if self.check_mcz_summary(sum_mc_f):
-                                        self.make_images_results_mc(hemi, analysis_name,
-                                                                    contrast, direction, 
-                                                                    cwsig_mc_f, oannot_mc_f)
-
-    def check_maxvox(self, glmdir, fsgd_type_contrast):
-        val = [i.strip() for i in open(path.join(glmdir, fsgd_type_contrast, 'maxvox.dat')).readlines()][0].split()[0]
-        if float(val) > 3.0 or float(val) < -3.0:
-            return True
-        else:
-            return False
+            print(f"    \n\n\n{len(ls_img[ls_img.index(sig):])} images LEFT for extraction")
 
 
-    def check_mcz_summary(self, file):
-        if len(linecache.getline(file, 42).strip('\n')) > 0:
-            return True
-        else:
-            return False
+    # def read_glm_subdirs(self):
+    #     files_glm = load_json(self.param.files_for_glm)
+    #     for fsgd_type in files_glm:
+    #         for fsgd_file in files_glm[fsgd_type]['fsgd']:
+    #             fsgd_f_unix = path.join(self.PATHglm, 'fsgd_unix', '{}_unix.fsgd'.format(fsgd_file.replace('.fsgd','')))
+    #             for hemi in hemispheres:
+    #                 for meas in self.measures:
+    #                     mcz_meas = self.param.GLM_MCz_meas_codes[meas]
+    #                     for thresh in self.threshs:
+    #                         analysis_name = '{}.{}.{}.fwhm{}'.format(fsgd_file.replace('.fsgd',''), meas, hemi, str(thresh))
+    #                         glmdir = path.join(self.PATHglm_glm, analysis_name)
+    #                         for contrast_file in files_glm[fsgd_type]['mtx']:
+    #                             fsgd_type_contrast = contrast_file.replace('.mtx','')
+    #                             contrast = fsgd_type_contrast.replace(fsgd_type+'_','')
+    #                             path_2contrast = path.join(glmdir, fsgd_type_contrast)
+    #                             if self.check_maxvox(glmdir, fsgd_type_contrast):
+    #                                 print(f"    Creating image for FDR corrected results")
+    #                                 self.make_images_results_fdr(hemi, glmdir, analysis_name, fsgd_type_contrast)
+    #                             for direction in self.mcz_sim_direction:
+    #                                 mcz_header  = 'mc-z.{}.{}{}'.format(direction, mcz_meas, str(self.mc_cache_thresh))
+    #                                 sum_mc_f    = path.join(path_2contrast,'{}.sig.cluster.summary'.format(mcz_header))
+    #                                 cwsig_mc_f  = path.join(path_2contrast,'{}.sig.cluster.mgh'.format(mcz_header))
+    #                                 oannot_mc_f = path.join(path_2contrast,'{}.sig.ocn.annot'.format(mcz_header))
+    #                                 if self.check_mcz_summary(sum_mc_f):
+    #                                     print(f"    Creating image for Monte-Carlo corrected results")
+    #                                     self.make_images_results_mc(hemi, analysis_name,
+    #                                                                 contrast, direction, 
+    #                                                                 cwsig_mc_f, oannot_mc_f)
+
+
+    # def check_maxvox(self, glmdir, fsgd_type_contrast):
+    #     val = [i.strip() for i in open(path.join(glmdir, fsgd_type_contrast, 'maxvox.dat')).readlines()][0].split()[0]
+    #     if float(val) > 3.0 or float(val) < -3.0:
+    #         return True
+    #     else:
+    #         return False
+
+
+    # def check_mcz_summary(self, file):
+    #     if len(linecache.getline(file, 42).strip('\n')) > 0:
+    #         return True
+    #     else:
+    #         return False
 
     def make_images_results_mc(self, hemi, analysis_name,
                                     contrast, direction,
@@ -178,7 +190,7 @@ class SaveGLMimages():
         # with open(self.f_with_cmds,'w') as f:
         #     for line in tksurfer_cmds:
         #         f.write(line+'\n')
-        # print('tksurfer fsaverage {} inflated -overlay {} -fthresh {} -tcl {}'.format(
+        # print('    tksurfer fsaverage {} inflated -overlay {} -fthresh {} -tcl {}'.format(
         #         hemi, path.join(glmdir, fsgd_type_contrast, sig_file), str(self.fdr_thresh), self.f_with_cmds))
         system('tksurfer fsaverage {} inflated -overlay {} -fthresh {} -tcl {}'.format(
                 hemi, path.join(glmdir, fsgd_type_contrast, sig_file), str(self.fdr_thresh), self.f_with_cmds))
