997640a191b934395b4e000d6e6d37cf6818112b
alex hanganu <alexandru.hanganu@umontreal.ca>
2022-08-03 09:01:52 -0700

    multi adj in laterality, for group analysis and lobe analysis

--- nimb/stats/stats_laterality.py
+++ nimb/stats/stats_laterality.py
@@ -32,20 +32,21 @@ def LateralityAnalysis(df,
     return df_lat
 
 
-def plot_laterality_per_group(ls_of_columns,
+def plot_laterality_per_group(ls_of_features,
                               data_2plot_per_group,
                               path_2save_img,
                               on_axis = "X",
                               y_axis_label = 'Laterality Index, right < 0 > left',
                               x_axis_label = 'Regions',
                               plot_title = 'Laterality Index by region, by group',
+                              plot_figure_size = (20, 3),
                               dpi = 150,
                               show = False):
     """creates a plot for laterality analysis
         author: 1st version by Andréanne Bernatchez 202205
                 adjusted by Emmanuelle Mazur-Lainé 202207, Alexandru Hanganu
     Args:
-        ls_of_columns = list() of pandas.DataFrame.columns
+        ls_of_features = list() of pandas.DataFrame.columns
         data_2plot_per_group = {"group_name": numpy.array(values_to_be_plotted)}
         on_axis = axis chosen to plot the data; default is "X", alternative is "Y"
         path_2save_img = absolute path to save the image
@@ -56,8 +57,9 @@ def plot_laterality_per_group(ls_of_columns,
     Return:
         saves plot
     """
+    plt.rcParams["figure.figsize"] = plot_figure_size
     groups = list(data_2plot_per_group.keys())
-    axis_vals = np.arange(len(ls_of_columns))
+    axis_vals = np.arange(len(ls_of_features))
 
     for group in data_2plot_per_group:
         ax_distance = 0.1 * groups.index(group)+0.1
@@ -68,11 +70,11 @@ def plot_laterality_per_group(ls_of_columns,
             plt.barh(axis_vals, data_2plot_per_group[group], 0.2, label = group)
 
     if on_axis == "X":
-        plt.xticks(axis_vals, ls_of_columns, rotation='vertical')
+        plt.xticks(axis_vals, ls_of_features, rotation='vertical')
         plt.xlabel(x_axis_label)
         plt.ylabel(y_axis_label)
     else:
-        plt.yticks(axis_vals, ls_of_columns)#, rotation='vertical')
+        plt.yticks(axis_vals, ls_of_features)
         plt.ylabel(x_axis_label)
         plt.xlabel(y_axis_label)
 
@@ -80,10 +82,11 @@ def plot_laterality_per_group(ls_of_columns,
     plt.grid(True, color = "grey", linewidth = "0.3", linestyle = "-")
     plt.legend()
     plt.savefig(path_2save_img, bbox_inches='tight', dpi=dpi)
+    plt.close()
 
 
 def get_lateralized_feats(feats,
-                         lat_param_left,
+                         first_lat_param,
                          lat_param_right,
                          print_check = False):
     '''create dict() with lateralized data
@@ -91,56 +94,56 @@ def get_lateralized_feats(feats,
         e.g.: <roi>_<hemi_param_left> = <roi>_<hemi_param_right>
     Args:
         feats = list() of all features
-        lat_param_left = str() for left part laterality parameter, e.g., lh or rh
+        first_lat_param = str() for left part laterality parameter, e.g., lh or rh
         lat_param_right = str() for right part laterality parameter, e.g., lh or rh
         print_check = if True will print the results of lateralized features
     Return:
         {roi: (left_roi, right_roi)}
     '''
 
-    def laterality_roi_get(feat, lat_param_left):
-        """will search if lat_param_left in feat
+    def laterality_roi_get(feat, first_lat_param):
+        """will search if first_lat_param in feat
         Args:
             feat = str()
-            lat_param_left = str()
+            first_lat_param = str()
         Return:
-            lat_feat_left: str() of feat[:lat_param_left]
+            first_lat_feat: str() of feat[:first_lat_param]
                     or: ""
         """
-        if lat_param_left in feat:
-            lat_feat_left = feat[:feat.find(lat_param_left)]
+        if first_lat_param in feat:
+            first_lat_feat = feat[:feat.find(first_lat_param)]
         else:
-            print("lat param: ", lat_param_left, " not in feature: ", feat)
-            lat_feat_left = ""
-        return lat_feat_left
+            first_lat_feat = ""
+        return first_lat_feat
 
-    def laterality_find_contra(feats, lat_feat_left):
+    def laterality_find_contra(feats, first_lat_feat):
         """searches for a potential contralateral feature
             in the list of all features
         Args:
             feats = list() of all feats
-            lat_feat_left = str() of a part of one feature that might be present in feats
+            first_lat_feat = str() of a part of one feature that might be present in feats
         Return:
             bool, roi_contra
         """
         contra_feat = ""
         for feat in feats:
-            if lat_feat_left in feat:
+            if first_lat_feat in feat:
                 contra_feat = feat
         return contra_feat
 
     lhrh_feat_d = {}
     contra_feats = list()
+    print(f"features to lateralize are: {feats}")
     for feat in feats:
         if feat not in contra_feats:
-            lat_feat_left = laterality_roi_get(feat, lat_param_left)
-            if lat_feat_left:
-                lat_feat_right = laterality_find_contra(feats, lat_feat_left)
+            first_lat_feat = laterality_roi_get(feat, first_lat_param)
+            if first_lat_feat:
+                lat_feat_right = laterality_find_contra(feats, first_lat_feat)
                 if lat_feat_right:
-                    lhrh_feat_d[lat_feat_left] = (feat, lat_feat_right)
+                    lhrh_feat_d[first_lat_feat] = (feat, lat_feat_right)
                     contra_feats.append(lat_feat_right)
             else:
-                print("lat param2: ", lat_feat_left, " not in feature: ", feat)
+                print("lat param: ", first_lat_param, " not in feature: ", feat)
     if print_check:
         print("CHECKING laterality:")
         for key in lhrh_feat_d:
@@ -148,61 +151,33 @@ def get_lateralized_feats(feats,
     return lhrh_feat_d
 
 
-def get_feats_lateralized_per_lobe(hemis,
-                                   lobes,
-                                   lateralized_lhrh):
-    """aims to create a lateralized dict with features classified per lobe
+def get_all_combined_feats_per_param(feats_per_param,
+                                     laterality_param):
+    """combines all features in a list of features
+        based on the laterality_param provided
     Args:
-        hemis = list(hemisphere_abbreviation_left, hemisphere_abbreviation_left)
-                similar to nimb/processing/atlases/atlas_definitions.hemis
-        lobes = list(of all lobes)
-                similar to nimb/processing/atlases/atlas_definitions.lobes.keys()
-        lateralized_lhrh = dict() from get_lateralized_feats()
+        feats_per_param = dict() {"feature_per_param_a": (feature_lateral_hemi_a1, feature_lateral_hemi_a2),
+                                  "feature_per_param_b": (feature_lateral_hemi_b1, feature_lateral_hemi_b2)}
+        laterality_param = list() [laterality_param_hemi1, laterality_param_hemi2]
     Return:
-        {lobe_hemi: [features,]}
+    ombined_feats_per_param = dict() {feature_per_param_laterality_param_hemi1: [feature_lateral_hemi_a1, feature_lateral_hemi_b1],
+                                      feature_per_param_laterality_param_hemi2: [feature_lateral_hemi_a2, feature_lateral_hemi_b2]}
     """
-    feats_per_lobe = dict()
-    for hemi in hemis:
-        pos = hemis.index(hemi)
-        for feat in lateralized_lhrh:
-            feat_hemi = lateralized_lhrh[feat][pos]
-            feat_ok = False
-            for lobe in lobes:
-                lobe_hemi = f"{lobe}_{hemi}"
-                if lobe_hemi not in feats_per_lobe:
-                    feats_per_lobe[lobe_hemi] = list()
-                if lobe in feat:
-                    feats_per_lobe[lobe_hemi].append(feat_hemi)
-                    feat_ok = True
-                    break
-                else:
-                    feat_ok = False
-            if not feat_ok:
-                print("not feat: ", feat_hemi)
-    return feats_per_lobe
-
-
-def get_df_data_per_lobe_per_group(dict_dfs_per_groups,
-                                    feats_per_lobe):
-    """will take the pandas.DataFrames per group
-        extract the corresponding features per lobes
-        as per features classified with get_feats_lateralized_per_lobe()
-    Args:
-        dict_dfs_per_groups = {group1: pandas.DataFrame,
-                               group2: pandas.DataFrame}
-        feats_per_lobe = dict() received from get_feats_lateralized_per_lobe()
-    Return:
-        dict_dfs_per_groups = {group1: pandas.DataFrame with feats per lobes,
-                               group2: pandas.DataFrame with feats per lobes}
-    """
-    d_dfs_per_group_per_lobe = dict()
-    for group in dict_dfs_per_groups:
-        df = dict_dfs_per_groups[group]
-        for lobe_hemi in feats_per_lobe:
-            cols = feats_per_lobe[lobe_hemi]
-            df[lobe_hemi] = df[cols].mean(axis = 1)
-        d_dfs_per_group_per_lobe[group] = df[list(feats_per_lobe.keys())]
-    return d_dfs_per_group_per_lobe
+    combined_feats_per_param = dict()
+    for param in feats_per_param:
+        feats = feats_per_param[param]
+        lateralized_lhrh = get_lateralized_feats(feats,
+                                                 laterality_param[0],
+                                                 laterality_param[1])
+        for hemi in laterality_param:
+            pos = laterality_param.index(hemi)
+            param_hemi = f"{param}_{hemi}"
+            if param_hemi not in combined_feats_per_param:
+                combined_feats_per_param[param_hemi] = list()
+            for feat_main in lateralized_lhrh:
+                feat_hemi = lateralized_lhrh[feat_main][pos]
+                combined_feats_per_param[param_hemi].append(feat_hemi)
+    return combined_feats_per_param
 
 
 def laterality_per_groups(dict_dfs_per_groups,
@@ -210,10 +185,12 @@ def laterality_per_groups(dict_dfs_per_groups,
                           lat_param_left,
                           lat_param_right,
                           path2save,
+                          ls_of_features_2plot = list(),
                           on_axis = "X",
                           plot_title = 'Laterality Index by region, by group',
+                          plot_figure_size = (20, 3),
                           dpi = 150,
-                          file_name = f'Laterality_results_',
+                          file_name = f'Laterality',
                           print_check = False,
                           show_img = False):
     """calculates laterality per multiple groups
@@ -227,6 +204,7 @@ def laterality_per_groups(dict_dfs_per_groups,
         lat_param_left = str() for left part laterality parameter, e.g., lh or rh
         lat_param_right = str() for right part laterality parameter, e.g., lh or rh
         path2save = absolute path to save the file file_name
+        ls_of_features_2plot = list() of features to use for plotting
         dpi = resolution, default is 150
         file_name = the name of the file to use for laterality data
         print_check = if True will print the results of lateralized_lhrh features
@@ -235,13 +213,14 @@ def laterality_per_groups(dict_dfs_per_groups,
         plot image
         saves csv files with laterality analysis
     """
+    groups = list(dict_dfs_per_groups.keys())
     lateralized_lhrh = get_lateralized_feats(feats,
                                         lat_param_left,
                                         lat_param_right, 
                                         print_check = False)
     laterality_calculated = {"means":dict()}
     for group in dict_dfs_per_groups:
-        file = f'{file_name}{group}'
+        file = f'{file_name}_results_{group}'
         df = dict_dfs_per_groups[group]
         laterality_results = LateralityAnalysis(df, 
                                                 lateralized_lhrh,
@@ -250,14 +229,15 @@ def laterality_per_groups(dict_dfs_per_groups,
         laterality_calculated[group] = laterality_results
         laterality_calculated["means"][group] = laterality_calculated[group].mean().to_numpy()
 
-    groups = list(dict_dfs_per_groups.keys())
-    X = laterality_calculated[groups[0]].columns.tolist()
-    X_axis = np.arange(len(X))
-    path_2save_img = os.path.join(path2save, f"laterality_{'_'.join(groups)}.png")
-    plot_laterality_per_group(X,
+
+    if not ls_of_features_2plot:
+        ls_of_features_2plot = laterality_calculated[groups[0]].columns.tolist()
+    path_2save_img = os.path.join(path2save, f"{file_name}_{'_'.join(groups)}.png")
+    plot_laterality_per_group(ls_of_features_2plot,
                               laterality_calculated["means"],
                               path_2save_img,
                               on_axis = on_axis,
                               plot_title = plot_title,
+                              plot_figure_size = plot_figure_size,
                               dpi = dpi,
                               show = show_img)
