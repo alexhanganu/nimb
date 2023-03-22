268c3f5091da1d5da7f2e920535b4083a950c3cb
alex hanganu <alexandru.hanganu@umontreal.ca>
2023-03-22 08:05:28 -0700

    moved to another file, the script for reading data from FreeSUrfer text files. Adjusted script that read the FreeSurfer version from the build file

--- nimb/processing/freesurfer/fs_stats2table.py
+++ nimb/processing/freesurfer/fs_stats2table.py
@@ -13,9 +13,10 @@ Return:
     an excel file with all subjects and all parameters, per sheets
     one big excel file with all parameters on one sheet
 '''
-from os import path, listdir, sep, system
+import os
 import sys
 import argparse
+from os import path, listdir, sep, system
 
 import pandas as pd
 import numpy as np
@@ -40,27 +41,31 @@ class FSStats2Table:
         fname_fs_per_param: one file with multiple sheets. Sheet per parameter per hemisphere, per atlas
     '''
     def __init__(self, ls_subjects,
-                    stats_DIR,
-                    project_vars,
-                    fs_ver,
+                    all_vars,
+                    project,
                     big_file = True,
                     use_params_nimb = True,
                     data_only_volumes=False,
                     new_date = True):
         self.ls_subjects       = ls_subjects
-        self.stats_DIR         = stats_DIR
-        self.project_vars      = project_vars
+        self.stats_DIR         = all_vars.params.stats_dir
+        self.project           = project
+        self.project_vars      = all_vars.projects[project]
         self.PATH2subjects     = self.project_vars["PROCESSED_FS_DIR"][1]
         self.big_file          = big_file
         self.data_only_volumes = data_only_volumes
         self.use_params_nimb   = use_params_nimb
-        self.fsver             = fs_ver#'7.2.0'
+        self.fs_home           = all_vars.location_vars['local']["FREESURFER"]["FREESURFER_HOME"]
+        user_fs_version        = all_vars.location_vars['local']["FREESURFER"]["version"]
+        self.fsver, _, _, _    = fs_definitions.fs_version(self.fs_home, user_fs_version)
+
         self.atlas_data        = atlas_definitions.atlas_data
         self.stats_files       = atlas_definitions.all_stats_files(self.fsver)
         self.miss              = dict()
-
+        logger.info(f"    FreeSurfer version is: {self.fsver}")
         self.define_file_names(new_date)
         self.define_atlas_criteria()
+        self.get_data          = fs_read_fs_textfile.TextDataGet()
         self.run()
 
 
@@ -115,40 +120,34 @@ class FSStats2Table:
         self.sheetnames_nimb  = list()
         self.row = 1
 
+        self.d_file_subjects = dict()
         for sub in self.ls_subjects:
-            subs_left = len(self.ls_subjects[self.ls_subjects.index(sub):])
-            logger.info(f'    reading: {sub}; left: {subs_left}')
             path_2sub        = self.get_path(self.PATH2subjects, sub)
-            ready           = self.chk_if_all_stats_present(sub,
-                                                            path_2sub)
-            if ready:
-                logger.info('    extracting stats for {}'.format(sub))
+            self.chk_if_all_stats_present(sub, path_2sub)
+        if self.d_file_subjects:
+            logger.info(f'ERR: multiple subject have missing files:')
+            for file in self.d_file_subjects:
+                logger.info(f'     file: {file}, is missing in {len(self.d_file_subjects[file])} participants')
+
+        logger.info(f'\n\n')
+        for sub in self.ls_subjects:
+            archived, _ = is_archive(sub)
+            if not archived:
+                subs_left = len(self.ls_subjects[self.ls_subjects.index(sub):])
+                path_2sub        = self.get_path(self.PATH2subjects, sub)
+                logger.info(f'    extracting stats for {sub}; left: {subs_left}')
                 self.get_fs_stats_2table(path_2sub, sub)
                 self.row += 1
+            else:
+                logger.info(f'subject: {sub} is archived. Please run:')
+                logger.info(f'    cd {self.fs_home}')
+                logger.info(f'    python nimb.py -project {self.project} -process run -do fs-get-stats')
         self.writer_fs.save()
         self.writer_nimb.save()
         self.save_missing()
+
         if self.big_file:
             self.make_one_sheet()
-
-        if self.miss:
-            self.ls_subjects = list(self.miss.keys())
-            self.writer_fs   = pd.ExcelWriter(self.dataf_fs_miss,   engine = 'xlsxwriter')
-            self.writer_nimb = pd.ExcelWriter(self.dataf_nimb_miss, engine = 'xlsxwriter')
-            self.sheetnames_nimb  = list()
-            self.row = 1
-            for sub in self.ls_subjects:
-                subs_left = len(self.ls_subjects[self.ls_subjects.index(sub):])
-                logger.info(f'\n    reading: {sub}; left: {subs_left}')
-                path_2sub        = self.get_path(self.PATH2subjects, sub)
-                logger.info('    extracting stats for {}'.format(sub))
-                self.get_fs_stats_2table(path_2sub, sub)
-                self.row += 1
-
-            self.writer_fs.save()
-            self.writer_nimb.save()
-            if self.big_file:
-                self.make_one_sheet()
 
 
     def chk_if_all_stats_present(self,
@@ -159,28 +158,17 @@ class FSStats2Table:
             _SUBJECT: sub to check, str
             path_2sub: path to _SUBJECT
         Return:
-            ready: _SUBJECT has all required files and is ready to undergo stats extraction
             populates self.miss with new missing _SUBJECT or missing files
         '''
-        def add_2dict(_SUBJECT, file):
-            if _SUBJECT not in self.miss:
-                self.miss[_SUBJECT] = list()
-            self.miss[_SUBJECT].append(file)
-
-        ready = True
-        archived, _ = is_archive(_SUBJECT)
-        if archived:
-            logger.info(f'subject: {_SUBJECT} is archived. Please run through nimb.py -project PROJECT_NAME -process run -do fs-get-stats')
-            ready = False
-        else:
-            for atlas in self.stats_files:
-                for file in self.stats_files[atlas]:
-                    if not path.isfile(path.join(path_2sub,
-                                                file)):
-                        logger.info(f'missing: {file}')
-                        ready = False
-                        add_2dict(_SUBJECT, file)
-        return ready
+        for atlas in self.stats_files:
+            for file in self.stats_files[atlas]:
+                if not path.isfile(path.join(path_2sub, file)):
+                    if _SUBJECT not in self.miss:
+                        self.miss[_SUBJECT] = list()
+                    self.miss[_SUBJECT].append(file)
+                    if file not in self.d_file_subjects:
+                        self.d_file_subjects[file] = list()
+                    self.d_file_subjects[file].append(_SUBJECT)
 
 
     def get_fs_stats_2table(self, path_2sub, sub):
@@ -199,15 +187,16 @@ class FSStats2Table:
                     self.f_stats_abspath = path.join(path_2sub, file)
                     logger.info(f'        using old version of {file}')
                 if self.f_exists(self.f_stats_abspath):
-                    content = ""
-                    try:
-                        content = list(open(self.f_stats_abspath,'r'))
-                    except Exception as e:
-                        print(e)
-                    if not content:
-                        print("ERR! file: ", self.f_stats_abspath, " is empty")
-                        break
-                    df, extra_measures = self.get_values(atlas, content, file, sub)
+                    # content = ""
+                    # try:
+                    #     content = list(open(self.f_stats_abspath,'r'))
+                    # except Exception as e:
+                    #     print(e)
+                    # if not content:
+                    #     print("ERR! file: ", self.f_stats_abspath, " is empty")
+                    #     break
+                    df, extra_measures = self.get_data.get_values(atlas, self.f_stats_abspath, file, sub)
+                    # df, extra_measures = self.get_values(atlas, content, file, sub)
                     parameters = self.atlas_data[atlas]['parameters']
                     if len(parameters) > 1:
                         for fs_param in parameters:
@@ -219,64 +208,12 @@ class FSStats2Table:
                             df2[sub] = df[fs_param]
                             df2.index = df['StructName']
                             df2 = df2.transpose()
-                            df2 = self.populate_extra_measures(df2, content, fs_param, extra_measures, atlas)
+                            df2 = self.populate_extra_measures(df2, fs_param, extra_measures, atlas)
                             self.add_sheet_2df(df2, sheetName, header_fs2nimb_dict, atlas)
                     else:
                         param = list(parameters.keys())[0]
                         sheetName = f'{atlas}_{param}_{hemisphere}'
                         self.add_sheet_2df(df, sheetName, header_fs2nimb_dict, atlas)
-
-
-    def get_values(self, atlas, content, file_name, sub):
-        # alternatively, code written by nipy/nibabel probably Chris Markiewicz @effigies
-        # with open(file_path, 'r') as f:
-        # -        for line in f:
-        # -            if re.findall(r'ColHeaders .*', line):
-        # -                column_names = line.split()[2:]
-        # -                break
-        # -    f.close()
-        # -    stats = np.loadtxt(file_path, comments='#', dtype=str)
-        if atlas in self.nuclei_atlases:
-            if '.v12' in file_name or '.v21' in file_name:
-                new_version = False
-                new_files = ('segmentHA_T1.sh',
-                            'segmentThalamicNuclei.sh',
-                            'segmentBS.sh')
-                for file in new_files:
-                    if file in content[0]:
-                        new_version = True
-                if new_version:
-                    values = {i.split(' ')[-1].strip('\n'):i.split(' ')[-2] for i in content[1:]}
-                else:
-                    values = {i.split(' ')[-2]:i.split(' ')[-1].strip('\n') for i in content}
-            elif "Hypothalamic" in content[0]:
-                values = {i.split(" ")[-1].strip("\n"):i.split(" ")[-3] for i in content}
-                values.pop("Stats",None)
-            else:
-                df = self.read_BS_HIP_v10(sub)
-
-            if values:
-                df = pd.DataFrame(values, index=[sub])
-            else:
-                logger.info(f'    ERROR: file {file} has NO content')
-                index_df = self.atlas_data[atlas]['header']
-                # index_df = fs_definitions.all_data[stats_param]['header']
-                df=pd.DataFrame(np.repeat('nan',len(index_df)), columns=[sub], index=index_df)
-                df=df.T
-        else:
-            values_raw = content[content.index([x for x in content if 'ColHeaders' in x][0]):]
-            if len(self.atlas_data[atlas]["hemi"]) < 2:
-            # if not fs_definitions.all_data[stats_param]["two_hemi"]:
-                values = [values_raw[0].split()[4:]]
-                for line in values_raw[1:]:
-                    values.append(line.split()[2:])
-            else:
-                values = [values_raw[0].split()[2:]]
-                for line in values_raw[1:]:
-                    values.append(line.split())
-            df = pd.DataFrame(values[1:], columns=values[0])
-        extra_measures = self.get_extra_measures(atlas, content)
-        return df, extra_measures
 
 
     def define_parameter_for_sheet(self, parameters, fs_param):
@@ -286,45 +223,7 @@ class FSStats2Table:
         return param
 
 
-    def read_BS_HIP_v10(self, sub):
-        '''to read old version FS 6 and older of the brainstem and hippocampus
-        stats file
-        '''
-        df=pd.read_table(self.f_stats_abspath)
-        df.loc[-1]=df.columns.values
-        df.index=df.index+1
-        df=df.sort_index()
-        df.columns=['col']
-        df['col'],df[sub]=df['col'].str.split(' ',1).str
-        df.index=df['col']
-        del df['col']
-        return df.transpose()
-
-
-    def get_extra_measures(self, atlas, content):
-        '''aparc files have a list of additional parameters in the
-        lines that start with the word "Measure"
-        this scripts extract those parameters in a dict()
-        Args:
-            content that was extracted with open
-        Return:
-            {Region_Parameter: value}, where Region = e.g., Cortex, Parameter = e.g., NumVert
-            value = float'''
-        d = dict()
-        for line in content:
-            if 'Measure' in line:
-                line_ls  = line.split()
-                region   = line_ls[2].strip(',')
-                fs_param = line_ls[3].strip(',')
-                value    = line_ls[-2].strip(',')
-                if atlas in self.criteria_4subcort:
-                    d[fs_param] = value
-                else:
-                    d[f'{region}_{fs_param}'] = value
-        return d
-
-
-    def populate_extra_measures(self, df, content, fs_param, measures, atlas):
+    def populate_extra_measures(self, df, fs_param, measures, atlas):
         '''additional parameters extracted with the
         get_extra_measures
         are bieng added to corresponding sheets in the xlsx file
@@ -443,6 +342,95 @@ class FSStats2Table:
         with open(f, 'w') as jf:
             json.dump(d, jf, indent=4)
 
+    # MOVED TO fs_read_fs_textfile on 20230318. Goal was to make on file that would read all FreeSurfer text files
+    # def get_values(self, atlas, content, file_name, sub):
+    #     # alternatively, code written by nipy/nibabel probably Chris Markiewicz @effigies
+    #     # with open(file_path, 'r') as f:
+    #     # -        for line in f:
+    #     # -            if re.findall(r'ColHeaders .*', line):
+    #     # -                column_names = line.split()[2:]
+    #     # -                break
+    #     # -    f.close()
+    #     # -    stats = np.loadtxt(file_path, comments='#', dtype=str)
+    #     if atlas in self.nuclei_atlases:
+    #         values = ""
+    #         if '.v12' in file_name or '.v21' in file_name:
+    #             new_version = False
+    #             new_files = ('segmentHA_T1.sh',
+    #                         'segmentThalamicNuclei.sh',
+    #                         'segmentBS.sh')
+    #             for file in new_files:
+    #                 if file in content[0]:
+    #                     new_version = True
+    #             if new_version:
+    #                 values = {i.split(' ')[-1].strip('\n'):i.split(' ')[-2] for i in content[1:]}
+    #             else:
+    #                 values = {i.split(' ')[-2]:i.split(' ')[-1].strip('\n') for i in content}
+    #         elif "Hypothalamic" in content[0]:
+    #             values = {i.split(" ")[-1].strip("\n"):i.split(" ")[-3] for i in content}
+    #             values.pop("Stats",None)
+    #         else:
+    #             df = self.read_BS_HIP_v10(sub)
+
+    #         if values:
+    #             df = pd.DataFrame(values, index=[sub])
+    #         else:
+    #             logger.info(f'    ERROR: file {file_name} has NO content')
+    #             index_df = self.atlas_data[atlas]['header']
+    #             df=pd.DataFrame(np.repeat('nan',len(index_df)), columns=[sub], index=index_df)
+    #             df=df.T
+    #     else:
+    #         values_raw = content[content.index([x for x in content if 'ColHeaders' in x][0]):]
+    #         if len(self.atlas_data[atlas]["hemi"]) < 2:
+    #         # if not fs_definitions.all_data[stats_param]["two_hemi"]:
+    #             values = [values_raw[0].split()[4:]]
+    #             for line in values_raw[1:]:
+    #                 values.append(line.split()[2:])
+    #         else:
+    #             values = [values_raw[0].split()[2:]]
+    #             for line in values_raw[1:]:
+    #                 values.append(line.split())
+    #         df = pd.DataFrame(values[1:], columns=values[0])
+    #     extra_measures = self.get_extra_measures(atlas, content)
+    #     return df, extra_measures
+
+
+    # def read_BS_HIP_v10(self, sub):
+    #     '''to read old version FS 6 and older of the brainstem and hippocampus
+    #     stats file
+    #     '''
+    #     df=pd.read_table(self.f_stats_abspath)
+    #     df.loc[-1]=df.columns.values
+    #     df.index=df.index+1
+    #     df=df.sort_index()
+    #     df.columns=['col']
+    #     df['col'],df[sub]=df['col'].str.split(' ',1).str
+    #     df.index=df['col']
+    #     del df['col']
+    #     return df.transpose()
+
+
+    # def get_extra_measures(self, atlas, content):
+    #     '''aparc files have a list of additional parameters in the
+    #     lines that start with the word "Measure"
+    #     this scripts extract those parameters in a dict()
+    #     Args:
+    #         content that was extracted with open
+    #     Return:
+    #         {Region_Parameter: value}, where Region = e.g., Cortex, Parameter = e.g., NumVert
+    #         value = float'''
+    #     d = dict()
+    #     for line in content:
+    #         if 'Measure' in line:
+    #             line_ls  = line.split()
+    #             region   = line_ls[2].strip(',')
+    #             fs_param = line_ls[3].strip(',')
+    #             value    = line_ls[-2].strip(',')
+    #             if atlas in self.criteria_4subcort:
+    #                 d[fs_param] = value
+    #             else:
+    #                 d[f'{region}_{fs_param}'] = value
+    #     return d
 
 
 def get_parameters(projects):
@@ -483,13 +471,13 @@ def get_parameters(projects):
 if __name__ == "__main__":
 
     try:
-        from pathlib import Path
+        import pathlib
+        file = pathlib.Path(__file__).resolve()
+        top = file.parents[2]
     except ImportError as e:
-        print('please install pathlib')
-        sys.exit(e)
+        print('please install pathlib', e)
+        "/".join(os.path.abspath(file_).split("/")[:-3])
 
-    file = Path(__file__).resolve()
-    parent, top = file.parent, file.parents[2]
     sys.path.append(str(top))
 
     from setup.get_vars import Get_Vars
@@ -498,18 +486,18 @@ if __name__ == "__main__":
     from distribution.manage_archive import is_archive
     from processing.atlases import atlas_definitions
     import fs_definitions
+    import fs_read_fs_textfile
 
     project_ids  = Get_Vars().get_projects_ids()
     params       = get_parameters(project_ids)
     all_vars     = Get_Vars(params)
-    project_vars = all_vars.projects[params.project]
-    fs_ver_long  = all_vars.location_vars['local']["FREESURFER"]["version"]
-    fs_ver       = fs_definitions.FreeSurferVersion(fs_ver_long).fs_ver()
+    project      = params.project
+    project_vars = all_vars.projects[project]
 
     if params.stats_dir == 'home':
         all_vars.params.stats_dir = project_vars["STATS_PATHS"]["STATS_HOME"]
     if params.dir_fs_stats != 'default':
-        project_vars["PROCESSED_FS_DIR"][1] = params.dir_fs_stats
+        all_vars.projects[project]["PROCESSED_FS_DIR"][1] = params.dir_fs_stats
     ls_subjects = params.list_of_subjects
     if ls_subjects:
         ls_subjects =  ls_subjects.split(",")
@@ -517,9 +505,8 @@ if __name__ == "__main__":
 
 
     FSStats2Table(ls_subjects,
-                    all_vars.params.stats_dir,
-                    project_vars,
-                    fs_ver,
+                    all_vars,
+                    project,
                     big_file = True,
                     use_params_nimb = True,
                     data_only_volumes=False,
